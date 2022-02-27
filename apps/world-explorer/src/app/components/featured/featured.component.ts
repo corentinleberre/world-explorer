50bb9da5dc2f723963a98b957d3ccc8cc5476a34
corentinleberre <corentin.leb29@gmail.com>
2022-02-27 21:12:55 +0100

    :sparkles: Ajustement automatique de la date

--- apps/world-explorer/src/app/components/featured/featured.component.ts
+++ apps/world-explorer/src/app/components/featured/featured.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { AirportCode, airports } from '../../utils/airport-code.util';
+import * as moment from 'moment';
 
 @Component({
   selector: 'world-explorer-featured',
@@ -10,25 +11,25 @@ import { AirportCode, airports } from '../../utils/airport-code.util';
 })
 export class FeaturedComponent {
 
-  public faMagnifyingGlass = faMagnifyingGlass;
-
-  public formGroup: FormGroup;
-
-  public airports: AirportCode[] = airports;
-
   @Input()
   public isLoading: boolean = false;
 
   @Output()
   public formSubmitEvent = new EventEmitter<any>();
 
-  constructor(private _formBuilder: FormBuilder){
-    this.formGroup = this._formBuilder.group({
-      people1: ['', Validators.required],
-      people2: ['', Validators.required],
-      start: ['', Validators.required],
-      end: ['', Validators.required]
-    });
+  public faMagnifyingGlass = faMagnifyingGlass;
+
+  public moment = moment;
+
+  public formGroup!: FormGroup;
+
+  public airports: AirportCode[] = airports;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.formGroup = this._buildFormGroup();
+    this.formGroup.controls['start'].valueChanges.subscribe(
+      (value) => this.formGroup.controls['end'].setValue(moment(value).add(1, 'days').format('YYYY-MM-DD'))
+    );
   }
 
   public selectEvent(item: AirportCode, controlName: string) {
@@ -36,10 +37,19 @@ export class FeaturedComponent {
   }
 
   public onSubmit(): void {
-    if(this.formGroup.valid) {
+    if (this.formGroup.valid) {
       console.log(this.formGroup.value);
       this.formSubmitEvent.emit(this.formGroup.value);
     }
+  }
+
+  private _buildFormGroup(): FormGroup {
+    return this._formBuilder.group({
+      people1: ['', Validators.required],
+      people2: ['', Validators.required],
+      start: [moment().format('YYYY-MM-DD'), Validators.required],
+      end: [moment().add(1, 'days').format('YYYY-MM-DD'), Validators.required]
+    });
   }
 
 }
